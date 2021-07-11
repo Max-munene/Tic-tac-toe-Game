51d284b8ef8a4c2105f36cd7bebf3470ab1459d5
MERN-Max <munenemaxwell2@gmail.com>
2021-07-12 00:17:36 +0300

    added darkmode

--- js/GameView.js
+++ js/GameView.js
@@ -1,48 +1,39 @@
+export default class GameView {
+	constructor() {
+		console.log('init game view');
+	}
 
-export default class GameView{
+	updateBoard(game) {
+		this.updateTurn(game);
 
-  constructor(){
-    console.log('init game view');
-  }
-  
-  updateBoard(game){
-    
-    this.updateTurn(game);
+		const winningCombination = game.findWinningCombinations();
 
-    const winningCombination = game.findWinningCombinations();
-    
+		for (let i = 0; i < game.board.length; i++) {
+			const tile = document.querySelector(`.board-tile[data-index='${i}']`);
+			tile.textContent = game.board[i];
 
-    for(let i=0; i < game.board.length; i++){
-      const tile = document.querySelector(`.board-tile[data-index='${i}']`);
-     tile.textContent = game.board[i];
+			tile.classList.remove('tile-winner');
 
-     tile.classList.remove("tile-winner");
+			let tileType = game.board[i] == 'X' ? 'tile-x' : 'tile-o';
 
-     let tileType = game.board[i] == 'X' ? "tile-x": "tile-o";
+			tile.innerHTML = `<span class="${tileType}">${game.board[i] ? game.board[i] : ''}</span>`;
 
-     tile.innerHTML= `<span class="${tileType}">${game.board[i] ? game.board[i]: ""}</span>`
+			if (winningCombination && winningCombination.includes(i)) {
+				tile.classList.add('tile-winner');
+			}
+		}
+	}
 
+	updateTurn(game) {
+		let playerX = document.querySelector('.player-x');
+		let playerO = document.querySelector('.player-o');
+		playerX.classList.remove('active');
+		playerO.classList.remove('active');
 
-
-     if(winningCombination && winningCombination.includes(i)){
-       tile.classList.add("tile-winner");
-      
-     }
-     
-    }
-  }
-  
-  updateTurn(game){
-     let playerX = document.querySelector(".player-x");
-     let playerO = document.querySelector(".player-o");
-     playerX.classList.remove('active');
-     playerO.classList.remove('active');
-
-     if(game.turn =='X'){
-       playerX.classList.add('active');
-    } else{
-      playerO.classList.add('active');
-    }
-  }
-
-} 
+		if (game.turn == 'X') {
+			playerX.classList.add('active');
+		} else {
+			playerO.classList.add('active');
+		}
+	}
+}
--- js/main.js
+++ js/main.js
@@ -1,34 +1,34 @@
-import Game from './Game.js'
-import GameView from './GameView.js'
-
+import Game from './Game.js';
+import GameView from './GameView.js';
 
 let game = new Game();
 let gameView = new GameView();
 
-document.querySelector(".restart").addEventListener("click",() => {
-  console.log("New Game");
-  onRestartClick();
-})
+document.querySelector('.restart').addEventListener('click', () => {
+	console.log('New Game');
+	onRestartClick();
+});
 
-let tiles = document.querySelectorAll(".board-tile");
-tiles.forEach((tile) =>{
-  tile.addEventListener("click", ()=>{
-   onTileClick(tile.dataset.index);
-  })
+let tiles = document.querySelectorAll('.board-tile');
+tiles.forEach((tile) => {
+	tile.addEventListener('click', () => {
+		onTileClick(tile.dataset.index);
+	});
+});
 
-})
-
-function onTileClick(i){
-
-  game.makeMove(i);//make a move 
-  gameView.updateBoard(game);//update booard
-  
-  
+function onTileClick(i) {
+	game.makeMove(i); //make a move
+	gameView.updateBoard(game); //update booard
 }
 
-function onRestartClick(){
-  game = new Game();
-  gameView.updateBoard(game);
+function onRestartClick() {
+	game = new Game();
+	gameView.updateBoard(game);
 }
 
 gameView.updateBoard(game);
+
+const checkbox = document.getElementById('checkbox');
+checkbox.addEventListener('change', () => {
+	document.body.classList.toggle('dark');
+});
